f324daf1d6dbd3b326c11e19e9e688d5783130b3
Trevor McCann <trevormccann21@gmail.com>
2017-09-26 20:00:17 -0700

    Better placeholder site.

--- index.php
+++ index.php
@@ -1,2 +1,28 @@
-<h1> Coming Soon! </h1>
-<h2> follow along at my <a href='https://www.github.com/tmccann21'> github </a> </h2>
+
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="description" content="Software x Graphic Design">
+		<title>Trevor McCann</title>
+		<link rel="icon" href="res/logo.png"></link>
+		<link rel="stylesheet" href="style/style.css"></link>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="scripts/scroll.js"></script>
+	</head>
+  <body>
+    <div class="corner-ribbon top-left sticky shadow">Under Construction <br> follow along @ my <a href='https://github.com/tmccann21/portfolio-website' style='color:#888; text-decoration:none;' target='_blank'> github </a></div>
+    <div id='header'>
+      <div>
+        <h1> <a href='#about'>about me<a> | <a href='#projects'>projects<a> | contact</h1>
+        <!--<img src='res/headshotbw3.png' class='ring'></img>-->
+      </div>
+    </div>
+    <div id='about'>
+    </div>
+    <div id='projects'>
+    </div
+  </body>
+</html>
